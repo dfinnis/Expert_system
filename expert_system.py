573636f96771b75cfefb879fcf50fcd3a4ea6590
Drew Finnis <drewfinnis@gmail.com>
2020-05-03 20:05:09 +0200

    feature/read file comments removed

--- expert_system.py
+++ expert_system.py
@@ -2,56 +2,58 @@ import argparse
 import os
 
 def parse_args():
-    my_parser = argparse.ArgumentParser(description="Expert system solves propositional calculus.")
-    my_parser.add_argument('Filepath',
-                       metavar='filepath',
-                       type=str,
-                       help='provide a valid input file. Rules, facts and queries')
-    args = my_parser.parse_args()
-    filepath = args.Filepath
-    return filepath
+	my_parser = argparse.ArgumentParser(description="Expert system solves propositional calculus.")
+	my_parser.add_argument('Filepath',
+					   metavar='filepath',
+					   type=str,
+					   help='provide a valid input file. Rules, facts and queries')
+	args = my_parser.parse_args()
+	filepath = args.Filepath
+	return filepath
 
 class graph:
-    def __init__(self):
-        self.facts = []
-        self.initial_facts = []
-        self.queries = []
+	def __init__(self):
+		self.facts = []
+		self.initial_facts = []
+		self.queries = []
 
 class fact:
 
-    def __init__(self, symbol):
-        print("I, {}, am initialized".format(symbol))############
-        self.symbol = symbol
-        self.true = False
-        self.rules = []
+	def __init__(self, symbol):
+		print("I, {}, am initialized".format(symbol))############
+		self.symbol = symbol
+		self.true = False
+		self.rules = []
 
-    def add_rule(self, rule):
-        print("I am adding rule {}".format(rule))##############
-        self.rules.append(rule)
+	def add_rule(self, rule):
+		print("I am adding rule {}".format(rule))##############
+		self.rules.append(rule)
 
 def main():
-    try:
-        filepath = parse_args()
-        print(filepath)############
-        if not os.path.isfile(filepath):
-            print("Error: filepath invalid")
+	try:
+		filepath = parse_args()
+		print(filepath)############
+		if not os.path.isfile(filepath):
+			print("Error: filepath invalid")
 
-        # f = fact('A')
-        # f.add_rule('=> B')
-        # f.add_rule('=> C')
-        # print(f.rules)
+		# f = fact('A')
+		# f.add_rule('=> B')
+		# f.add_rule('=> C')
+		# print(f.rules)
 
-        with open(filepath, 'r') as file:
-            for line in file:
-                str = line.replace(" ", "").replace("\t", "").replace("\n", "")
-                if str != "":
-                    if str[0] != '#':
-                        print(str)
-                # print("oh yeh!")
+		with open(filepath, 'r') as file:
+			for line in file:
+				str = line.replace(" ", "").replace("\t", "").replace("\n", "").split("#")[0]
+				if str != "":
+					## if string has non sanctioned characters reject
+					# allowedSymbols_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ()+!|^=>?' # < "if and only if"
+					print(str)
+				# print("oh yeh!")
 
-        print("Oh hi!")######!!!!!
-    except:
-        print("Error")
+		print("Oh hi!")######!!!!!
+	except:
+		print("Error")
 
 if __name__ == '__main__':
-    main()
+	main()
+		
